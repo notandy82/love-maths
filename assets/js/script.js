78c2c11dda75ab018a1da117cdf41e650d5fd9c6
notandy82 <notandystanek@gmail.com>
2022-01-25 07:52:51 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame(params) {
+    
+}
+
+function checkAnswer(params) {
+    
+}
+
+function calculateCorrectAnswer(params) {
+    
+}
+
+function incrementScore(params) {
+    
+}
+
+function incrementWrongAnswer(params) {
+    
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion(params) {
+    
+}
+
+function displayMultiplyQuestion(params) {
+    
+}
